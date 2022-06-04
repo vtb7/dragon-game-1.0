cdbe9adbd7587752d1d66b4de8692de3c86c7f7c
vtb7 <88791316+vtb7@users.noreply.github.com>
2022-06-04 12:58:57 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,34 @@
+var dragon,dragonImg;
+var sky,skyImg;
+var archer,archerImg;
+var arrow,arrowImg;
+
+function preload(){
+  dragonImg=loadImage("/assets/Dragon.png")
+  skyImg=loadImage("/assets/Sky.jpg")
+  arrowImg=loadImage("/assets/Arrow.png")
+  archerImg=loadImage("/assets/Archer.png")
+}
+
+function setup() {
+  createCanvas(windowWidth,windowHeight)
+  sky=createSprite(200,200,windowWidth,windowHeight)
+  sky.addImage(skyImg)
+  sky.scale=0.7
+
+  dragon=createSprite(1317,239)
+  dragon.addImage(dragonImg)
+
+  archer=createSprite(150,300)
+  archer.addImage(archerImg)
+}
+
+function draw() {
+  console.log(mouseX,mouseY);
+  background(180);
+  drawSprites()
+    }
+  
+   
+     
+   
