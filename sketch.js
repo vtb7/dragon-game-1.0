4d041ab9d782346d0cccd6ad9354511dd1ee47f8
vtb7 <88791316+vtb7@users.noreply.github.com>
2022-06-11 12:18:59 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -14,8 +14,8 @@ function setup() {
   createCanvas(windowWidth,windowHeight)
   sky=createSprite(200,200,windowWidth,windowHeight)
   sky.addImage(skyImg)
-  sky.scale=0.7
-
+  sky.scale=0.9
+  sky.velocityX=-2
   dragon=createSprite(1317,239)
   dragon.addImage(dragonImg)
 
@@ -27,7 +27,18 @@ function draw() {
   console.log(mouseX,mouseY);
   background(180);
   drawSprites()
+  if (sky.x < 0){
+    sky.x = sky.width/3;
+  }
+  //archer movement
+  if(keyIsDown(UP_ARROW)) 
+  archer.y=archer.y-5
+
+  if(keyIsDown(DOWN_ARROW)) {
+    archer.y=archer.y+5
+  }
     }
+  
   
    
      
